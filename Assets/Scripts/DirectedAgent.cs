5cd916cd95c78f389fae44251482be153d7df93c
leane dupuis <calibandj@gmail.com>
2019-10-05 16:43:53 +0200

    pathfinding ennemy

---
+++ Assets/Scripts/DirectedAgent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class DirectedAgent : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+
+    private GameObject player = null;
+
+    private float nextActionTime = 0.0f;
+    public float period = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+            player = GameObject.Find("Player");
+    }
+
+
+            // Use this for initialization
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveToLocation()
+    {
+        agent.destination = player.transform.position;
+        agent.isStopped = false;
+    }
+
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            MoveToLocation();
+        }
+
+    }
+}
