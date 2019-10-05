04b9390105b93b6ace46e9dc1ca091cc7a11d675
unknown <morgan.boivin@gmail.com>
2019-10-05 15:16:47 +0200

    MBN trees + first house on WorldMap. Sample of wall

---
+++ Assets/TreeRandomizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRandomizer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Tree t = this.GetComponent<Tree>();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
