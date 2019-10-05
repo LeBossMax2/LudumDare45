6a5ead971feb083528eff55af5ae83456c2629f8
Lucas Guedon <guedon.lucas@laposte.net>
2019-10-05 17:33:10 +0200

    Add ennemy model

--- Assets/Scripts/Ennemy_Controller.cs
+++ Assets/Scripts/Ennemy_Controller.cs
@@ -2,40 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ennemy_Controller : MonoBehaviour
+public class Ennemy_Controller : Character
 {
     public int current_healthPoint = 0;
     // Time value
-    public float reloadDelay = 10;
     public float movementSpeed = 10;
     public int damageDone = 1;
 
-    public GameObject player;
-
-    private float nextActionTime = 0.0f;
-    public float period = 2.0f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (player == null)
-            player = GameObject.Find("Player");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Time.time > nextActionTime)
-        {
-            nextActionTime += period;
-            this.transform.position += player.transform.position;
-        }
-   
-    }
-
     public void die()
     {
-        this.Start();
+        Destroy(gameObject);
+    }
+
+    public override void damage(int value)
+    {
+        current_healthPoint -= value;
+        if (current_healthPoint == 0)
+        {
+            die();
+        }
     }
 }
