f8d67c6be037d51d6076385bb026483f302cf82a
Otoon <oliviernguyenyb@gmail.com>
2019-10-05 12:17:01 +0200

    Playler Movement
    add player movement

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -8,19 +8,36 @@ public class Controller : MonoBehaviour
     public int current_healthPoint = 0;
     // Time value
     public float reloadDelay = 10;
-    public float movementSpeed = 10;
+    public float movementSpeed = 169.0f;
     public int damageDone = 1;
+
+    public GameObject character;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            character.transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            character.transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            character.transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            character.transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        }
     }
 
     public void die()
