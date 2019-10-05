a7e8dbcfb1caf42e0a7d1ed36dc1a09415a58d6f
leane dupuis <calibandj@gmail.com>
2019-10-05 19:21:41 +0200

    damage ennemy

--- Assets/Scripts/Ennemy_Controller.cs
+++ Assets/Scripts/Ennemy_Controller.cs
@@ -9,6 +9,9 @@ public class Ennemy_Controller : Character
     public float movementSpeed = 10;
     public int damageDone = 1;
 
+    private float nextActionTime = 0.0f;
+    public float period = 1.0f;
+
     public void die()
     {
         Destroy(gameObject);
@@ -20,6 +23,19 @@ public class Ennemy_Controller : Character
         if (current_healthPoint == 0)
         {
             die();
+        }
+    }
+
+    private void OnCollisionStay(Collision col)
+    {
+        if (Time.time > nextActionTime)
+        {    
+            Character ch = col.gameObject.GetComponent<Character>();
+            if (ch != null && ch.isBad != isBad)
+            {
+                ch.damage(damageDone);
+            }
+                nextActionTime += period;
         }
     }
 }
