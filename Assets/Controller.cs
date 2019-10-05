1d3a3dfc593c59c99dca33eb0f64ebc0a3d80673
unknown <morgan.boivin@gmail.com>
2019-10-05 11:45:43 +0200

    MBN add controller script for player, add stats

---
+++ Assets/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public int max_healthPoint = 100;
+    public int current_healthPoint = 0;
+    // Time value
+    public float reloadDelay = 10;
+    public float movementSpeed = 10;
+    public int damageDone = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void die()
+    {
+        this.Start();
+    }
+}
