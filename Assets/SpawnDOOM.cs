e169ece813f864cb87e23a863844aa781df62497
unknown <morgan.boivin@gmail.com>
2020-11-10 16:24:47 +0100

    MBN DOOM hat code midified to prevent the ennemy counter to get negatives values

---
+++ Assets/SpawnDOOM.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpawnDOOM : MonoBehaviour
+{
+    protected float cd = 0;
+
+    public int ennemiesPerWave = 2;
+    public float timeToSpawnOneEnnemy = 10;
+    private int entGeneratedThisWave = 0;
+
+    public GameObject[] minions;
+    public int[] minionsProbas;
+    public int healPerMinion = 0;
+
+    public bool active = false;
+    public int localWaveNumber = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        cd += Time.deltaTime;
+        if (active)
+        {
+            Spawn();
+        }
+        else if (cd >= timeToSpawnOneEnnemy)
+        {
+            active = HudController.maxNumberEnnemies > (HudController.ennemiesCount);
+            cd = 0;
+        }
+    }
+
+    void Spawn()
+    {
+        if (cd >= timeToSpawnOneEnnemy)
+        {
+            GameObject badGuy = Instantiate(minions[Random.Range(0, minions.Length)], this.transform.position, Quaternion.identity);
+            Ranged_enemy_controllers hp = this.GetComponent<Ranged_enemy_controllers>();
+            if (hp != null)
+            {
+                badGuy.GetComponent<NavMeshAgent>().speed = this.GetComponent<NavMeshAgent>().speed * 1.25F;
+                badGuy.GetComponent<DirectedAgent>().period *= this.GetComponent<Ranged_enemy_directed_agent>().period * 0.8F;
+                badGuy.GetComponent<Ennemy_Controller>().damageDone += (int)Mathf.Max((hp.current_healthPoint*0.3F), localWaveNumber * 1.3F);
+                entGeneratedThisWave++;
+                hp.current_healthPoint+=healPerMinion;
+            }
+            cd = 0;
+            HudController.ennemiesCount++;
+            if (entGeneratedThisWave >= ennemiesPerWave)
+            {
+                localWaveNumber++;
+                entGeneratedThisWave = 0;
+            }
+        }
+    }
+}
