12ea39d161d631986229de01a49fc999670eba6e
unknown <morgan.boivin@gmail.com>
2020-11-10 12:01:45 +0100

    Small code to make things invisible when something comes near their hitbox

---
+++ Assets/disappearingThing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disappearingThing : MonoBehaviour
+{
+    public MeshRenderer[] meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Controller c = other.GetComponent<Controller>();
+        foreach (MeshRenderer mr in this.meshRenderer) {
+            mr.enabled = (false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Controller c = other.GetComponent<Controller>();
+        foreach (MeshRenderer mr in this.meshRenderer)
+        {
+            mr.enabled = true;
+        }
+    }
+}
